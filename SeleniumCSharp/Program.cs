508814f98eedc125abefb6f41393cafeb227a362
Eleks <msdn@eleks.com>
2019-04-08 17:15:34 +0300

    1

--- SeleniumCSharp/Program.cs
+++ SeleniumCSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,20 @@ namespace SeleniumCSharp
     {
         static void Main(string[] args)
         {
+           
+        }
+
+        [Test]
+        public void CompareTitle()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.i.ua/");
+
+            string title = "І.UA - твоя пошта";
+
+
+            Assert.AreEqual(title, driver.Title.Trim());
+            driver.Close();
         }
     }
 }
