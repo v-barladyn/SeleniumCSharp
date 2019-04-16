bc9c527d556dbabac6619dad3d840bf753a36162
Eleks <msdn@eleks.com>
2019-04-16 17:25:27 +0300

    1

--- SeleniumCSharp/Program.cs
+++ SeleniumCSharp/Program.cs
@@ -2,10 +2,9 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic; 
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
 
 namespace SeleniumCSharp
 {
@@ -13,53 +12,266 @@ namespace SeleniumCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter sum of  2 + 2 = ");          
 
-            Assert.That(2 + 2, Is.EqualTo(Convert.ToInt32(Console.ReadLine())));
         }
       
 
-        [Test]
-        public void CompareTitle()
+      
+    }
+    class Tests
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [OneTimeSetUp]        
+        public void Before()
         {
-            IWebDriver driver = new ChromeDriver();
+            
             driver.Navigate().GoToUrl("https://www.i.ua/");
+            driver.Manage().Window.Maximize();
 
-            string title = "І.UA - твоя пошта";
+        }
+        [SetUp]
+        public void BeforeEachTests()
+        {
+          
+        }
+
+        // Smoke Tests
+
+        [Test]
+        public void CheckIfLoginFormIsPresented()
+        {
+            IWebElement loginForm = driver.FindElement(By.Name("lform"));
+
+            Assert.IsTrue(loginForm.Displayed);
+
+        }
+
+        [Test]
+        public void VerifyTitleOfLoginForm()
+        {
+
+            IWebElement TitleOfLoginForm = driver.FindElement(By.LinkText("Пошта"));
+
+            Assert.IsTrue(TitleOfLoginForm.Displayed);
+
+        }
+
+        [Test]
+        public void LoginText()
+        {
+
+            IWebElement loginText = driver.FindElement(By.XPath("//p[contains(text(),'Логін')]"));
+
+            Assert.IsTrue(loginText.Displayed);
+
+        }
+        [Test]
+        public void LoginInput()
+        {
+
+            IWebElement loginInput = driver.FindElement(By.Name("login"));
+
+            Assert.IsTrue(loginInput.Displayed);
+
+        }
+
+        [Test]
+        public void PasswordText()
+        {
+
+            IWebElement passwordText = driver.FindElement(By.XPath("//p[contains(text(),'Пароль')]"));
+
+            Assert.IsTrue(passwordText.Displayed);
+
+        }
+        [Test]
+        public void PasswordInput()
+        {
+
+            IWebElement passwordInput = driver.FindElement(By.Name("pass"));
+
+            Assert.IsTrue(passwordInput.Displayed);
+
+        }
+
+        [Test]
+        public void RememberLink()
+        {
+
+            IWebElement rememberLink = driver.FindElement(By.LinkText("нагадати"));
+
+            Assert.IsTrue(rememberLink.Displayed);
+
+        }
+
+        [Test]
+        public void DropdownWithDomens()
+        {
+
+            IWebElement dropdownWithDomens = driver.FindElement(By.Name("domn"));
+
+            Assert.IsTrue(dropdownWithDomens.Displayed);
+
+        }
+
+        [Test]
+        public void RememberMeText()
+        {
+
+            IWebElement rememberMeText = driver.FindElement(By.CssSelector("#c00"));
+
+            Assert.IsTrue(rememberMeText.Displayed);
+
+        }
+
+        [Test]
+        public void RememberMeCheckBox()
+        {
+
+            IWebElement rememberMeCheckBox = driver.FindElement(By.Name("auth_type"));
+
+            Assert.IsTrue(rememberMeCheckBox.Displayed);
+
+        }
+        [Test]
+        public void RegistrationLink()
+        {
+
+            IWebElement registrationLink = driver.FindElement(By.LinkText("Реєстрація"));
+
+            Assert.IsTrue(registrationLink.Displayed);
+
+        }
+
+        [Test]
+        public void LoginButton()
+        {
+
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input[value='Увійти']"));
+
+            Assert.IsTrue(loginButton.Displayed);
+
+        }
 
 
-            Assert.AreEqual(title, driver.Title.Trim());
+        
+        //2.2 
+
+        [Test]
+        public void LogIntoMailBox()
+        {
+            IWebElement passwordInput = driver.FindElement(By.Name("pass"));
+            IWebElement loginInput = driver.FindElement(By.Name("login"));
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input[value='Увійти']"));
+
+            string myLogin = "Vasillsa@email.ua";
+            string myPassword = "qwerty123!";
+            string title = "Вхідні - I.UA ";
+
+            loginInput.SendKeys(myLogin);       
+            passwordInput.SendKeys(myPassword);
+            loginButton.Click();
+
+            Assert.AreEqual(title, driver.Title);
+        }
+
+        //2.3
+
+        [Test]
+        public void LogIntoMailBoxVerification()
+        {
+            IWebElement passwordInput = driver.FindElement(By.Name("pass"));
+            IWebElement loginInput = driver.FindElement(By.Name("login"));
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input[value='Увійти']"));
+            IWebElement dropdownWithDomens = driver.FindElement(By.Name("domn"));
+            IWebElement rememberMeCheckBox = driver.FindElement(By.Name("auth_type"));
+
+            SelectElement oSelect = new SelectElement(dropdownWithDomens);
+
+            string myLogin = "Vasillsa@email.ua";
+            string myPassword = "qwerty123!";
+            string title = "Вхідні - I.UA ";
+            string emailDomen = "email.ua";       
+                       
+            loginInput.SendKeys(myLogin);
+            passwordInput.SendKeys(myPassword);
+            oSelect.SelectByText(emailDomen);
+            rememberMeCheckBox.Click();            
+
+            Assert.AreEqual(emailDomen, oSelect.SelectedOption.Text);
+            Assert.IsTrue(rememberMeCheckBox.Selected);
+            Assert.AreEqual(myLogin, loginInput.GetAttribute("value"));
+            Assert.AreEqual(myPassword, passwordInput.GetAttribute("value"));           
+        }
+
+        //2.4
+        [Test]
+        public void VerifyEmail()
+        {
+            IWebElement passwordInput = driver.FindElement(By.Name("pass"));
+            IWebElement loginInput = driver.FindElement(By.Name("login"));
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input[value='Увійти']"));
+    
+            
+
+            string myLogin = "Vasillsa@email.ua";
+            string myPassword = "qwerty123!";
+            string welcomEmail = "Ласкаво просимо на I.UA!";            
+         
+
+            loginInput.SendKeys(myLogin);
+            passwordInput.SendKeys(myPassword);
+            loginButton.Click();
+
+            IWebElement welcomThemeEmail = driver.FindElement(By.XPath("//a/span/span[contains(text(),'Ласкаво просимо на I.UA!')]"));
+            
+            Assert.AreEqual(welcomEmail, welcomThemeEmail.Text);
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(welcomThemeEmail);
+            actions.Perform();
+        
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(2000));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//li[contains(text(),' Добрий день, vasyl vasyl.')]")));
+
+            IWebElement emailPopup = driver.FindElement(By.XPath("//li[contains(text(),' Добрий день, vasyl vasyl.')]"));
+            Assert.IsTrue(emailPopup.Displayed);
+         
+         
+        }
+
+        //2.5
+        [Test]
+        public void LogIntoMailBoxJSExecutor()
+        {
+            IWebElement passwordInput = driver.FindElement(By.Name("pass"));
+            IWebElement loginInput = driver.FindElement(By.Name("login"));
+            IWebElement loginButton = driver.FindElement(By.CssSelector("input[value='Увійти']"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor) driver;
+
+            string myLogin = "Vasillsa@email.ua";
+            string myPassword = "qwerty123!";
+            string title = "Вхідні - I.UA ";            
+
+            js.ExecuteScript("arguments[0].setAttribute('value', arguments[1])", loginInput, myLogin);
+            js.ExecuteScript("arguments[0].setAttribute('value', arguments[1])", passwordInput, myPassword);
+            js.ExecuteScript("arguments[0].click();", loginButton);         
+
+            Assert.AreEqual(title, driver.Title);
+        }
+
+
+        [TearDown]
+        public void AfterTests()
+        {
             driver.Close();
         }
 
-        [Test]
-        public void VerifyNumber()
-        {
-            Random rnd = new Random();
-            int rd = rnd.Next(0, 100);
-                       
-
-            Assert.That((rd <= 50) && (rd >= 10) && (CheckIfPrime(rd) == true), "not prime " + rd);
-            
-        }
-
-        public bool CheckIfPrime(int number)
-        {
-            if (number == 1 || number == 2)
-            {
-                return false;
-            }
 
 
-            if (number % 2 == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
+
     }
 
 
